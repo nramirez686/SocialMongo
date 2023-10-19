db30494057d14d7afdf30a0a65d7c08cecbe9619
Fancy Nancy <ramireznancy.686@gmail.com>
2023-10-19 09:34:18 -0600

    added files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const port = 3001;
+
+const connectionStringURI = `mongodb://127.0.0.1:27017`;
+
+const client = new MongoClient(connectionStringURI);
+
+let db;
+//need to update db name
+const dbName = "shelterDB";
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected successfully to MongoDB");
+    db = client.db(dbName);
+
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Mongo connection error: ", err.message);
+  });
+
+app.use(express.json());
